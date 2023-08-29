7de7885e67c591398b68f835085263fef07a36ba
innok <innokmm@gmail.com>
2023-08-29 18:29:41 +1200

    Adding apartments business domain;

--- BookMe.Domain/Apartments/Apartment.cs
+++ BookMe.Domain/Apartments/Apartment.cs
@@ -1,6 +1,32 @@
-﻿namespace BookMe.Domain.Apartments
+﻿namespace BookMe.Domain.Apartments;
+
+public sealed class Apartment
 {
-    public sealed class Apartment
-    {
-    }
+    public Guid Id { get; private set; }
+
+    public string Name { get; private set; }    
+
+    public string Description { get; private set; }
+
+    public string Country { get; private set; }
+
+    public string State { get; private set; }
+
+    public string ZipCode { get; private set; }
+
+    public string City { get; private set; }
+
+    public string Street { get; private set; }
+
+    public decimal PriceAmount { get; private set; }
+
+    public string PriceCurrency { get; private set; }
+
+    public decimal CleaningFeeAmount { get; private set; }
+
+    public decimal CleaningFeeCurrency { get; private set; }
+
+    public DateTime? LastBookedOnUtc { get; private set; }
+
+    public List<Amenity> Amenities { get; private set; }
 }
